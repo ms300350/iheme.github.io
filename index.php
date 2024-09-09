97896fae3d21e1acc8e7ed362037caa167a77029
ms300350 <21footycomp@gmail.com>
2024-09-09 12:01:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php // Silence is golden
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to https://www.youtube.com</title>
+<meta http-equiv="refresh" content="0; URL=https://www.youtube.com">
+<link rel="canonical" href="https://www.youtube.com">
